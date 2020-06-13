ec44fd3203546dd6ec8f0fa7e7e008e49867d5cf
Abdul Sohail <abdul@systumbdi.com>
2020-06-14 02:53:30 +0530

    adding internationalization for english and malay

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module ContactPortal
 
     config.assets.paths << Rails.root.join("vendor")
 
+    config.i18n.available_locales = [:en, :ms] 
+    config.i18n.default_locale = :en
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	root to: 'contacts#new'
-	get '/contact', to: 'contacts#new'
+  	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do 
+  		root to: 'contacts#new'
+		get '/contact', to: 'contacts#new'
 
-	#redirect everything to /contact.
-	get '*path' => redirect('/contact')
+		#redirect everything to /contact.
+		get '*path' => redirect('/contact')
+	end
 	namespace :api, defaults: { format: 'json' } do
 		post '/save_contact', to: 'contacts#save_contact'
 	end
